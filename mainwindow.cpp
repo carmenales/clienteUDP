6b25e452b7aebb304dbe2754bca915ded55804db
carmenales <carmen.aleslopez@gmail.com>
2016-12-08 16:14:56 +0100

    subiendo archivos al repositorio

---
+++ mainwindow.cpp
@@ -0,0 +1,19 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_sendButton_clicked(){
+    int port;
+    port = ui->puerto->text().toInt(&ok,10);
+}
